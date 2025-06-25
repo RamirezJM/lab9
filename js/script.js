7aa3fde39b0c234e901b9cb2c7efc8f5277d7a95
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-06-24 22:55:33 -0400

    script update, style adjustment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function init(){
-  var button = document.getElementById('entrybutton');
+  const button = document.getElementById('entrybutton');
 
   function showMeText(){
-    var textbox = document.getElementById('entryinput');
+    const textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("RamirezJM: " + textbox.value);
   }
